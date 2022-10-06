8b6407cfbace10f8fc6bef167d3ddb175ce1848c
YesntDabnt <113894089+YesntDabnt@users.noreply.github.com>
2022-10-07 01:05:28 +0200

    yes

--- path/assignments file/assignments.js
+++ path/assignments file/assignments.js
@@ -3,7 +3,7 @@ function changeThing(){
 
     let displayImage = document.getElementById('image2')
     if(displayImage.src.match('https://www.freeiconspng.com/thumbs/pokeball-png/pokeball-icon-download-23.png')){
-        displayImage.src = '/övningar/openball.png'
+        displayImage.src = '/bilder/openball.png'
     }   
     else{
         displayImage.src = 'https://www.freeiconspng.com/thumbs/pokeball-png/pokeball-icon-download-23.png'
