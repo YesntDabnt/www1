bc593003b80db475af85b1aa7ed0bcd816121465
YesntDabnt <113894089+YesntDabnt@users.noreply.github.com>
2022-11-07 11:02:15 +0100

    fff

--- python/test.py
+++ python/test.py
@@ -1,9 +1,10 @@
-import urllib.request
-pdf_path = ""
-def download_file(download_url, filename):
-    response = urllib.request.urlopen(download_url)    
-    file = open(filename + ".pdf", 'wb')
-    file.write(response.read())
-    file.close()
- 
-download_file(pdf_path, "Test")
+def this_fails(text):
+    x = 1/0
+
+try:
+    this_fails()
+except ZeroDivisionError as err:
+    print('Handling run-time error:', err)
+
+
+this_fails(1)
